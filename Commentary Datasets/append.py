c384050aa324af60c5625ab7d3e4f4a018a5ff27
daplf <daplf96@gmail.com>
2018-11-21 01:58:49 +0000

    One more thing

--- Commentary Datasets/append.py
+++ Commentary Datasets/append.py
@@ -2,14 +2,21 @@ import csv
 import json
 import glob
 import os
+import codecs
+import io
+
+def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
+    csv_reader = csv.reader(utf8_data, dialect=dialect, **kwargs)
+    for row in csv_reader:
+        yield [unicode(cell, 'utf-8') for cell in row]
 
 for filename in glob.glob('Refined/*.csv'):
     csvfile = os.path.splitext(filename)[0]
     jsonfile = csvfile + '.json'
 
     with open(csvfile+'.csv') as f:
-        reader = csv.DictReader(f, delimiter=';')
-        rows = list(reader)
+		reader = unicode_csv_reader(f, delimiter=';')
+		rows = list(reader)
 
-    with open(jsonfile, 'w') as f:
-        json.dump(rows, f)
+    with codecs.open(jsonfile, 'w', encoding='utf-8') as f:
+        json.dump(rows, f, ensure_ascii=False)
